a66630fd61561869d8e662d960626d5ade4c75cb
Errorist <71728280+Errorist79@users.noreply.github.com>
2025-07-14 18:40:59 +0300

    feat(logging): add expected attestation block to epoch start logs

--- src/state.rs
+++ src/state.rs
@@ -107,6 +107,8 @@ impl State {
                 .get_attestation_info(operational_address)
                 .await
                 .context("Getting attestation info")?;
+            let expected_attestation_block =
+                attestation_info.calculate_expected_attestation_block();
             tracing::info!(
                 staker_address=?attestation_info.staker_address,
                 operational_address=?attestation_info.operational_address,
@@ -115,6 +117,7 @@ impl State {
                 epoch_start=%attestation_info.current_epoch_starting_block,
                 epoch_length=%attestation_info.epoch_len,
                 attestation_window=%attestation_info.attestation_window,
+                expected_attestation_block=%expected_attestation_block,
                 "New epoch started"
             );
 
